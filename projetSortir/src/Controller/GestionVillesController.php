66fffec87a5d6d5b46c29dfc36193d2eb2a58ec6
Sokhlar <Sokhlar@hotmail.fr>
2022-03-07 12:02:18 +0100

    - Ajout du front pour la gestion des villes.

--- projetSortir/src/Controller/GestionVillesController.php
+++ projetSortir/src/Controller/GestionVillesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VilleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class GestionVillesController extends AbstractController
     /**
      * @Route("/gestion/villes", name="gestion_villes")
      */
-    public function index(): Response
+    public function index( VilleRepository $villeRepository): Response
     {
+        $villes = $villeRepository->findAll();
         return $this->render('gestion_villes/index.html.twig', [
             'controller_name' => 'GestionVillesController',
+            'villes' => $villes
         ]);
     }
 }
