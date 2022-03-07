405024b4b5896d08db9dc424dd141676fed55ca7
Sokhlar <Sokhlar@hotmail.fr>
2022-03-07 12:11:40 +0100

    - Ajout du front pour la gestion des sites.

--- projetSortir/src/Controller/GestionSitesController.php
+++ projetSortir/src/Controller/GestionSitesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class GestionSitesController extends AbstractController
     /**
      * @Route("/gestion/sites", name="gestion_sites")
      */
-    public function index(): Response
+    public function index(SiteRepository $siteRepository): Response
     {
+        $sites = $siteRepository->findAll();
         return $this->render('gestion_sites/index.html.twig', [
             'controller_name' => 'GestionSitesController',
+            'sites' => $sites
         ]);
     }
 }
