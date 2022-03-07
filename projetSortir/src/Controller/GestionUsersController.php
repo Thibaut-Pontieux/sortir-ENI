a85265fdb710e44a3d5998ee0cfc0fd052e6d478
Sokhlar <Sokhlar@hotmail.fr>
2022-03-07 11:26:58 +0100

    - Ajout du front avec condition pour la gestion des users.

--- projetSortir/src/Controller/GestionUsersController.php
+++ projetSortir/src/Controller/GestionUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class GestionUsersController extends AbstractController
     /**
      * @Route("/gestion/users", name="gestion_users")
      */
-    public function index(): Response
+    public function index(ParticipantRepository $participantRepository): Response
     {
+        $users = $participantRepository->findAll();
         return $this->render('gestion_users/index.html.twig', [
             'controller_name' => 'GestionUsersController',
+            'users' => $users
         ]);
     }
 }
